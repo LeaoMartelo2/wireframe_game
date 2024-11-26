378d6ba2f17c70fa61bf96d169f42d75bf65a7de
LeaoMartelo <leaomartelo@gmail.com>
2024-11-26 01:06:29 -0300

    moved maps in to .map files @TODO: make map generator/editor tool

--- src/models.c
+++ src/models.c
@@ -8,38 +8,7 @@
 
 void load_map(Solid *solids, int *count, Ground *ground_geometry) {
 
-    int solid_count = 0;
-
-    Ground floor = {0};
-    floor.size = (Vector3){2000, 1, 2000};
-    floor.mesh = GenMeshCube(floor.size.x, floor.size.y, floor.size.z);
-    floor.model = LoadModelFromMesh(floor.mesh);
-    floor.pos = (Vector3){0, 0, 0};
-    ground_geometry[0] = floor;
-
-    Solid wall;
-    wall.size = (Vector3){300, 100, 10};
-    wall.mesh = GenMeshCube(wall.size.x, wall.size.y, wall.size.z);
-    wall.model = LoadModelFromMesh(wall.mesh);
-    wall.pos = (Vector3){200, 52, 200};
-    wall.see_through = false;
-
-    solids[0] = wall;
-    solid_count++;
-
-    Solid transparent_wall;
-    transparent_wall.size = (Vector3){10, 100, 300};
-    transparent_wall.mesh = GenMeshCube(transparent_wall.size.x, transparent_wall.size.y, transparent_wall.size.z);
-    transparent_wall.model = LoadModelFromMesh(transparent_wall.mesh);
-    transparent_wall.pos = (Vector3){600, 52, 200};
-    transparent_wall.see_through = true;
-
-    solids[1] = transparent_wall;
-    solid_count++;
-
-    *count = solid_count;
-
-    /*printf("%i\n", *count);*/
+#include "map_geometry.map"
 }
 
 void draw_map(const Solid *solids, int count, const Ground *ground_plates) {
@@ -66,65 +35,7 @@ void draw_map(const Solid *solids, int count, const Ground *ground_plates) {
 
 void load_props(Prop *props) {
 
-    Prop cube = {0};
-    cube.pos = (Vector3){0, 10, 0};
-    cube.size = (Vector3){10, 10, 10};
-    cube.mesh = GenMeshCube(cube.size.x, cube.size.y, cube.size.z);
-    cube.model = LoadModelFromMesh(cube.mesh);
-    cube.rotate = false;
-    cube.bounce = true;
-
-    props[0] = cube;
-
-    Prop static_cube = {0};
-    static_cube.pos = (Vector3){35, 10, 0};
-    static_cube.size = (Vector3){10, 10, 10};
-    static_cube.mesh = GenMeshCube(10, 10, 10);
-    static_cube.model = LoadModelFromMesh(static_cube.mesh);
-    static_cube.rotate = true;
-    static_cube.bounce = false;
-
-    props[1] = static_cube;
-
-    Prop third_cube = {0};
-    third_cube.pos = (Vector3){70, 10, 0};
-    third_cube.size = (Vector3){10, 10, 10};
-    third_cube.mesh = GenMeshCube(third_cube.size.x, third_cube.size.y, third_cube.size.z);
-    third_cube.model = LoadModelFromMesh(third_cube.mesh);
-    third_cube.rotate = true;
-    third_cube.bounce = true;
-
-    props[2] = third_cube;
-
-    Prop test = {0};
-    test.pos = (Vector3){110, 10, 0};
-    test.size = (Vector3){5, 5, 10};
-    test.mesh = GenMeshSphere(test.size.x, 5, test.size.z);
-    test.model = LoadModelFromMesh(test.mesh);
-    test.rotate = true;
-    test.bounce = true;
-
-    props[3] = test;
-
-    Prop cone = {0};
-    cone.pos = (Vector3){147, 10, 0};
-    cone.size = (Vector3){5, 10, 5};
-    cone.mesh = GenMeshCone(cone.size.x, cone.size.y, cone.size.z);
-    cone.model = LoadModelFromMesh(cone.mesh);
-    cone.rotate = true;
-    cone.bounce = true;
-
-    props[4] = cone;
-
-    Prop cylinder = {0};
-    cylinder.pos = (Vector3){184, 10, 0};
-    cylinder.size = (Vector3){5, 10, 5};
-    cylinder.mesh = GenMeshCylinder(cylinder.size.x, cylinder.size.y, cylinder.size.z);
-    cylinder.model = LoadModelFromMesh(cylinder.mesh);
-    cylinder.rotate = true;
-    cylinder.bounce = true;
-
-    props[5] = cylinder;
+#include "map_props.map"
 }
 
 double rot = 0;
