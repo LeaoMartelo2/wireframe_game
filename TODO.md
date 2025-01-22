79337171ec7a1d3714eb0928e3547f120e04e7ed
LeaoMartelo <leaomartelo@gmail.com>
2025-01-22 15:08:45 -0300

    this dosen't need to be scoped anymore

--- TODO.md
+++ TODO.md
@@ -1,5 +1,11 @@
- - [ ] - Add scene manager
- -   [ ] - move player in to scene manager, so the same player instance can be loaded between different scenes
+ - [X] - Add scene manager
+ - [X] - move player in to scene manager, so the same player instance can be loaded between different scenes
 
  - [ ] re-add props and moveable entities in the game
+ - [ ] add triggers for in game level loading
+
+
+
+ - [ ] improve movimentation code to avoid getting stuck at geometry (raycast the step maybe?)
+ - [ ] add a distance parameter to collision checking (no need to check collision on something far away)
 
--- src/add_scenes.cpp
+++ src/add_scenes.cpp
@@ -6,19 +6,17 @@ void SceneManager::add_scenes() {
 
     // setup each level specific data here;
 
-    {
-        auto test_level = std::make_shared<Scene>();
-        test_level->set_map("levels/test");
-        test_level->player = &player;
-        test_level->scene_id = 0;
-        test_level->start_pos = {150, 20, 50};
-        scenes.emplace_back(test_level);
+    std::shared_ptr test_level = std::make_shared<Scene>();
+    test_level->set_map("levels/test");
+    test_level->player = &player;
+    test_level->scene_id = LEVEL_TEST;
+    test_level->start_pos = {150, 20, 50};
+    scenes.emplace_back(test_level);
 
-        auto level2 = std::make_shared<Scene>();
-        level2->set_map("levels/level2");
-        level2->player = &player;
-        level2->scene_id = 1;
-        level2->start_pos = {150, 20, 50};
-        scenes.emplace_back(level2);
-    }
+    std::shared_ptr level2 = std::make_shared<Scene>();
+    level2->set_map("levels/level2");
+    level2->player = &player;
+    level2->scene_id = LEVEL_SAMPLE;
+    level2->start_pos = {150, 20, 50};
+    scenes.emplace_back(level2);
 }
