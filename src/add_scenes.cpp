d83006f47fac746c6e8475eb6b58526999004087
LeaoMartelo <leaomartelo@gmail.com>
2025-02-27 19:43:06 -0300

    cleanup

--- src/add_scenes.cpp
+++ src/add_scenes.cpp
@@ -14,20 +14,17 @@ void SceneManager::add_scenes() {
     test_level->set_map("levels/test");
     test_level->player = &player;
     test_level->scene_id = SCENE_LEVEL_TEST1;
-    test_level->start_pos = {105, 15, 50};
     scenes.emplace_back(test_level);
 
     Scene *level2 = new Scene;
     level2->set_map("levels/level2");
     level2->player = &player;
     level2->scene_id = SCENE_LEVEL_TEST2;
-    level2->start_pos = {150, 20, 50};
     scenes.emplace_back(level2);
 
     Scene *level3 = new Scene;
     level3->set_map("levels/level3");
     level3->player = &player;
     level3->scene_id = SCENE_LEVEL_TEST3;
-    level3->start_pos = {150, 20, 50};
     scenes.emplace_back(level3);
 }
