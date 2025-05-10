e6f1dc457538043dd6e350cfb0f3e9b990fa0f53
LeaoMartelo <leaomartelo@gmail.com>
2025-05-10 13:12:55 -0300

    test

---
+++ src/wireframe.h
@@ -0,0 +1,8 @@
+#ifndef WIREFRAME_RENDER_H_
+#define WIREFRAME_RENDER_H_
+
+#include "../raylib/raylib.h"
+#include "../raylib/raymath.h"
+#include "../raylib/rlgl.h"
+
+#endif // !WIREFRAME_RENDER_H_
